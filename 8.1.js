6c1c5337b17c26baadb22c48a563abc6c0b3f1aa
Sparrow <wroblewski.m@int.pl>
2024-10-15 18:02:17 +0200

    8.1.1

--- 8.1.js
+++ 8.1.js
@@ -4,6 +4,6 @@ const decodedComp = decodeURIComponent(secretMes1);
 console.log(decodedComp);
 const encodeComp = encodeURIComponent(secretMes2);
 console.log(encodeComp);
-const uri = "http://www.basescripts.com?=Witaj, świecie!";
+const uri = "http://www.basescripts.com?=Witaj,świecie!";
 const encoded = encodeURI(uri);
 console.log(encoded);
